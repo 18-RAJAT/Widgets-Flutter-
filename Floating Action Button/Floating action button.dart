6effcdedad375c185f26390b402b3910ebee1b79
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-29 16:29:06 +0530

    Add files via upload

---
+++ Floating Action Button/Floating action button.dart
@@ -0,0 +1,10 @@
+
+    floatingActionButton: FloatingActionButton.extended(
+      onPressed: () {
+        // Add your onPressed code here!
+      },
+      label: const Text('Approved'),
+      icon: const Icon(Icons.thumb_up),
+      backgroundColor: Colors.teal,
+
+    ),
